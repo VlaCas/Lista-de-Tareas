d85820889107ff1003d860b5c4b8ce90c081fefc
VlaCas <34453102+VlaCas@users.noreply.github.com>
2023-02-06 17:09:06 -0400

    Actualización 1.1
    Correcciones.

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+let altTar = 6
 crearTarea = () => {
-    let contenedor = document.getElementsByClassName("contenedor")[0];
-    listTar = document.createElement("ol");
-    let newTar = document.createElement("textArea");
-    newTar.setAttribute("class", "tarea");
-    newTar.setAttribute("placeHolder", "Nueva tarea...")
-    contenedor.appendChild(listTar);
-    listTar.appendChild(newTar);
-}
+    altTar = altTar + 2
+    document.getElementsByClassName("contenedor")[0].style.height = altTar + "rem";
+    setTimeout(() => {
+        let contenedor = document.getElementsByClassName("contenedor")[0];
+        listTar = document.createElement("ol");
+        let newTar = document.createElement("textArea");
+        newTar.setAttribute("class", "tarea");
+        newTar.setAttribute("placeHolder", "Nueva tarea...");
+        newTar.setAttribute("rows", "1");
+        contenedor.appendChild(listTar);
+        listTar.appendChild(newTar);
+        newTar.focus();
+    }, 0500);
+};
