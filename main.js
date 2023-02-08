4658a6ccb0beaf534e00454742824f14b16f4fe7
VlaCas <34453102+VlaCas@users.noreply.github.com>
2023-02-08 18:33:07 -0400

    Actualización 1.4
    Agregada la opcion de cambiar de tema oscuro a tema claro y viceversa.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let altTar = 6
-unaList = true
+let unaList = true
+let colPar = "white"
+
 crearTarea = () => {
 
     altTar = altTar + 1.4
@@ -26,7 +28,8 @@ crearTarea = () => {
                 p.setAttribute("class", "parrafos");
                 listTar.replaceChild(p, newTar);
                 p.innerText = newTar.value;
-                if (p.innerText.length == 0) {p.innerText = "Nueva Tarea..."} 
+                p.style = "color: " + colPar
+                if (p.innerText.length == 0) {p.innerText = "Nueva tarea..."} 
                 else if (p.innerText.length >= 23 && window.innerWidth <= 350) {document.getElementsByClassName("contenedor")[0].style.height = altTar + 1 + "rem"; altTar = altTar + 1}
                 console.log(p.innerText.length)}});
 
@@ -37,6 +40,7 @@ crearTarea = () => {
                 if (text.length >= 23 && window.innerWidth <= 350) {document.getElementsByClassName("contenedor")[0].style.height = altTar + 1 + "rem"}
                 let p = document.createElement("p");
                 p.innerText = text;
+                p.style = "color: " + colPar
                 p.setAttribute("class", "parrafos");
                 let bot = document.createElement("button");
                 let bot2 = document.createElement("button");
@@ -47,5 +51,37 @@ crearTarea = () => {
                 listTar.replaceChild(p, newTar)}})}, 0700)}
 
 let cambiarTema = () => {
-    document.body.style = "background: radial-gradient(at center bottom, rgb(191,211,232) -35%, rgb(126,170,199) 70%); transition: background 1s ease;"
-}
+    if (colPar == "white") { 
+        colPar = "rgb(61, 81, 139)";
+        document.body.style.background = "radial-gradient(at center bottom, rgb(191,211,232) -35%, rgb(126,170,199) 70%)";
+        document.getElementsByTagName("div")[0].style.background = "rgba(255, 255, 255, .25)";
+        document.getElementsByTagName("h1")[0].style = "color: #5370a7 ";
+        document.getElementsByTagName("button")[0].style = "background: rgba(255, 255, 255, 0.40)";
+        document.getElementsByTagName("svg")[0].style = "fill: none; stroke: #5370a7;";
+        document.getElementsByTagName("button")[1].style = "background: rgba(255, 255, 255, 0.40)";
+        svg2 = document.getElementsByTagName("svg")[1];
+        svg2line1 = svg2.getElementsByTagName("line")[0];
+        svg2line2 = svg2.getElementsByTagName("line")[1];
+        svg2line1.setAttribute("stroke", "#5370a7");
+        svg2line2.setAttribute("stroke", "#5370a7");
+        document.querySelector("hr").style = "background-color: rgb(128, 151, 203)";
+        parrafos = document.querySelectorAll("p");
+        for (i = 0; i < parrafos.length; i++) {
+            parrafos[i].style = "color: rgb(61, 81, 139);"}}
+    else {
+        colPar = "white";
+        document.body.style.background = "radial-gradient(at center bottom, rgb(0, 63, 205) -35%, rgb(1, 2, 8) 70%)";
+        document.getElementsByTagName("div")[0].style.background = "rgba(0, 0, 0, .4)";
+        document.getElementsByTagName("h1")[0].style = "color: white";
+        document.getElementsByTagName("button")[0].style = "background: rgba(255, 255, 255, .08)";
+        document.getElementsByTagName("svg")[0].style = "fill: #fff; stroke: #fff;";
+        document.getElementsByTagName("button")[1].style = "background: rgba(255, 255, 255, .08)";
+        svg2 = document.getElementsByTagName("svg")[1];
+        svg2line1 = svg2.getElementsByTagName("line")[0];
+        svg2line2 = svg2.getElementsByTagName("line")[1];
+        svg2line1.setAttribute("stroke", "#fff");
+        svg2line2.setAttribute("stroke", "#fff");
+        document.querySelector("hr").style = "background-color: rgba(255, 255, 255, .1)";
+        parrafos = document.querySelectorAll("p");
+        for (i = 0; i < parrafos.length; i++) {
+            parrafos[i].style = "color: white"}}}
